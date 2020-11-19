4df78398f272124d491927f379503dc9dd844114
Adarsh <adarsh1772000@gmail.com>
2020-11-19 21:46:19 +0530

    Members and Projects dummy screens added

---
+++ lib/screens/members_package/members_botnav.dart
@@ -0,0 +1,24 @@
+import 'package:dsc_iiitdmkl/ThemeData/fontstyle.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MembersBotNav extends StatefulWidget{
+
+  _MembersBotNavState createState() => _MembersBotNavState();
+}
+
+class _MembersBotNavState extends State<MembersBotNav> with TickerProviderStateMixin{
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
---
+++ lib/screens/projects_package/projects_botnav.dart
@@ -0,0 +1,26 @@
+import 'package:dsc_iiitdmkl/ThemeData/fontstyle.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProjectsBotNav extends StatefulWidget{
+
+  _ProjectsBotNavState createState() => _ProjectsBotNavState();
+}
+
+class _ProjectsBotNavState extends State<ProjectsBotNav> with TickerProviderStateMixin{
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlueAccent,
+    );
+  }
+
+
+}
